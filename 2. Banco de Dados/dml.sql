d8913a50531eca577aa17fb23759bb7cc0abf914
David Douglas Gonçalves Ribeiro <daviddouglas1042@mail.com>
2022-06-07 11:38:24 -0300

    Consultar funcionando

--- 2. Banco de Dados/dml.sql
+++ 2. Banco de Dados/dml.sql
@@ -2,3 +2,10 @@ use INFOAprovinhaHeroiBD;
 
 insert into tb_super_heroi(nm_super_heroi, ds_super_poder, bt_voa)
 		values ('Brunex', 'Ninja da programação', false);
+        
+        
+select id_super_heroi id,
+	   nm_super_heroi nome,
+       ds_super_poder superPoder,
+       bt_voa		  voa
+from tb_super_heroi;
